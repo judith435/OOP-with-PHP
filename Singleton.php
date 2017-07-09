a8824dde63657558c2386830fb2b248246fe6786
yaakov hatam <kobieshka@gmail.com>
2017-07-09 14:55:30 +0300

    Create Singleton.php

---
+++ Singleton.php
@@ -0,0 +1,27 @@
+<?php
+final class UserFactory
+{
+    /**
+     * Call this method to get singleton
+     *
+     * @return UserFactory
+     */
+    public static function Instance()
+    {
+        static $inst = null;
+        if ($inst === null) {
+            $inst = new UserFactory();
+        }
+        return $inst;
+    }
+
+    /**
+     * Private ctor so nobody else can instance it
+     *
+     */
+    private function __construct()
+    {
+
+    }
+}
+?>
