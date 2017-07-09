8eb8f6cb5988df75ce3b00188922b5bfe42cb493
yaakov hatam <kobieshka@gmail.com>
2017-07-09 14:39:51 +0300

    Create Rectangle.php

---
+++ Rectangle.php
@@ -0,0 +1,16 @@
+class Rectangle implements Shape {
+  private $width;
+  private $height;
+   
+  public function __construct($width, $height)
+  {
+    $this -> width = $width;
+    $this -> height = $height;
+  }
+  
+  // calcArea calculates the area of rectangles   
+  public function calcArea()
+  {
+    return $this -> width * $this -> height;
+  }
+}
