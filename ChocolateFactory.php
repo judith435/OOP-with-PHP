bc48760c66305f121147257dc774fc53c9c62849
yaakov hatam <kobieshka@gmail.com>
2017-07-09 14:54:11 +0300

    Create ChocolateFactory.php

---
+++ ChocolateFactory.php
@@ -0,0 +1,36 @@
+<?php
+class Chocolate
+{
+    private $chocolateName;
+    private $receipt;
+
+    public function __construct($name, $receipt)
+    {
+        $this->cocolateName = $name;
+        $this->vehicleModel = $receipt;
+    }
+
+    public function getReceipt()
+    {
+        return $this->name. ': '. $this->receipt;
+    }
+}
+
+class WillyWonkaChocolateFactory
+{
+    public static function create($chocolateName, $receipt)
+    {
+        return new Chocolate($chocolateName, $receipt);
+    }
+}
+
+// have the factory create the Chocolate object
+$snicky = WillyWonkaChocolateFactory::create('Snickers', array (
+  2 => 'sugar',
+  3 => 'chocolate',
+  4 => 'caramel',
+  121 => 'milk',
+));
+
+print_r($snicky->getReceipt()); // outputs "Bugatti Veyron"
+?>
